31520961e3684f924d17dfba2654f62bfd8bf3c2
guihkx <626206+guihkx@users.noreply.github.com>
2025-01-06 07:59:10 -0300

    windows: attach a console to show debug output

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,18 @@
 #include <QApplication>
 #include <QFontDatabase>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+		freopen("CONOUT$", "w", stdout);
+		freopen("CONOUT$", "w", stderr);
+	}
+#endif
     QApplication app(argc, argv);
     // Set application information
     app.setApplicationName("Notes");
